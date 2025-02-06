c865ca81749cf6f629115e904e73f94e891b19a6
Acapulco-1 <acapulco.1@buckeyemail.osu.edu>
2025-02-06 15:20:11 -0500

    Adding Sprite Factory and Sprite for Player Character. Unfinished, but should still run

---
+++ Sprint2/Sprites/ZeldaSprite.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint0.Sprites
+{
+    public class ZeldaSprite : ISprite
+    {
+        private Texture2D _texture;
+        Rectangle sourceRectangle;
+
+        public ZeldaSprite(Texture2D texture, int spriteSheetXPos, int spriteSheetYPos, int frame, int state)
+        {
+            _texture = texture;
+            switch (state)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+            switch (frame)
+            {
+                case 1:
+                    sourceRectangle = new Rectangle(16, 16, spriteSheetXPos, spriteSheetYPos);
+                    break;
+                case 2:
+                    sourceRectangle = new Rectangle(16, 16, spriteSheetXPos + 17, spriteSheetYPos);
+                    break;
+                case 3:
+                    sourceRectangle = new Rectangle(16, 16, spriteSheetXPos + 34, spriteSheetYPos);
+                    break;
+                case 4:
+                    sourceRectangle = new Rectangle(16, 16, spriteSheetXPos + 51, spriteSheetYPos);
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //Fill
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // no-op
+        }
+
+    }
+}
--- Sprint2/Sprites/ZeldaSpriteFactory1.cs
+++ Sprint2/Sprites/ZeldaSpriteFactory1.cs
@@ -1,1 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Sprites;
 
+namespace SpriteFactory
+{
+    public class ZeldaSpriteFactory
+    {
+        private Vector2 _position
+        private Texture2D zeldaSpriteSheet;
+        private enum ZeldaStates { GreenSmallShield, GreenMagicalShield, WhiteSmallShield, WhiteMagicalShield, RedSmallShield, RedMagicalShield };
+        private ZeldaStates currentState = ZeldaStates.GreenSmallShield;
+
+        private static ZeldaSpriteFactory instance = new ZeldaSpriteFactory();
+
+        public static ZeldaSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private ZeldaSpriteFactory()
+        {
+        }
+
+        public void LoadZeldaTextures(ContentManager spriteBatch)
+        {
+            zeldaSpriteSheet = spriteBatch.Load<Texture2D>("Zelda");
+        }
+        /*
+        public ISprite CreateDownIdle()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 1, 11);
+        }
+
+        public ISprite CreateUpIdle()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 69, 11);
+        }
+
+        public ISprite CreateLeftIdle()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 35, 11);
+        }
+
+        public ISprite CreateRightIdle()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 35, 11);
+        }
+        */
+        public ISprite CreateDownWalk(int frame, int state)
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 1, 11, frame, state);
+        }
+
+        public ISprite CreateUpWalk(int frame, int state)
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 69, 11, frame, state);
+        }
+
+        public ISprite CreateLeftWalk(int frame, int state)
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 35, 11, frame, state);
+        }
+
+        public ISprite CreateRightWalk(int frame, int state)
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 35, 11, frame, state);
+        }
+
+        public ISprite CreateUseItemDown(int frame, int state)
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 107, 11, frame, state);
+        }
+
+        public ISprite CreateUseItemUp()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 141, 11);
+        }
+
+        public ISprite CreateUseItemLeft()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 124, 11);
+        }
+
+        public ISprite CreateUseItemRight()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 124, 11);
+        }
+
+
+        public ISprite CreatePickUpItemOne()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 213, 11);
+        }
+
+        public ISprite CreatePickUpItemTwo()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 230, 11);
+        }
+
+
+        public ISprite CreateDownAttackWoodenSword(int frame)
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 1, 47, frame);
+        }
+
+        public ISprite CreateUpAttackWoodenSword(int frame)
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 1, 108, frame);
+        }
+
+        public ISprite CreateLeftAttackWoodenSword(int frame)
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 1, 77, frame);
+        }
+
+        public ISprite CreateRightAttackWoodenSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 1, 77);
+        }
+
+
+        public ISprite CreateDownAttackWhiteSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 94, 47);
+        }
+
+        public ISprite CreateUpAttackWhiteSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 94, 108);
+        }
+
+        public ISprite CreateLeftAttackWhiteSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 94, 77);
+        }
+
+        public ISprite CreateRightAttackWhiteSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 94, 77);
+        }
+
+
+        public ISprite CreateDownAttackMagicalSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 187, 47);
+        }
+
+        public ISprite CreateUpAttackMagicalSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 187, 108);
+        }
+
+        public ISprite CreateLeftAttackWMagicalSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 187, 77);
+        }
+
+        public ISprite CreateRightAttackMagicalSword()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 187, 77);
+        }
+
+
+        public ISprite CreateDownAttackMagicalRod()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 32, 32);
+        }
+
+        public ISprite CreateUpAttackMagicalRod()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 32, 32);
+        }
+
+        public ISprite CreateLeftAttackWMagicalRod()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 32, 32);
+        }
+
+        public ISprite CreateRightAttackMagicalRod()
+        {
+            return new ZeldaSprite(zeldaSpriteSheet, 32, 32);
+        }
+    }
+}
