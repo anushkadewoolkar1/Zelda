8da88b3d466a2b4dca76b1bc36ba15d7a77e3f68
Chloe Feller <feller.75@osu.edu>
2025-02-08 16:23:19 -0500

    Add files via upload

---
+++ Sprint2/Enemy.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Sprites;
+
+namespace Sprint0.States
+{
+    public class Enemy : IEnemyState
+    {
+        private EnemySprite sprite;
+        private EnemySpriteFactory spriteFactory;
+        private EnemyStateMachine stateMachine;
+
+        public Enemy()
+        {
+            // start out in moving state
+            stateMachine = new EnemyStateMachine();
+        }
+
+        public void ChangeEnemy()
+        {
+            stateMachine.ChangeEnemy();
+        }
+
+        public double SetHealth()
+        {
+            // do switch case here based on what the enemy's name is (similar to GetState method methinks)
+            return 0;
+        }
+        
+        public void Load(SpriteBatch spriteBatch)
+        {
+            sprite = spriteFactory.CreateEnemySprite();
+        }
+
+
+        public void Move(Enemy enemy)
+        {
+            stateMachine.Moving(enemy);
+        }
+
+        public void TakeDamage()
+        {
+            stateMachine.TakeDamage();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            stateMachine.Update();
+
+            sprite.Update(gameTime);
+        }
+
+        public void DrawCurrentSprite(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+
+        public void ChangeState(EnemyStateMachine enemyState)
+        {
+            stateMachine.Stop();
+            stateMachine = enemyState;
+            // stateMachine.Load(sprite);
+
+        }
+    }
+}
