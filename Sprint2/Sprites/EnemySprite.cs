f8a92d55fe520a09d990200295cf5a28d4c76c8d
Chloe Feller <feller.75@osu.edu>
2025-02-08 16:28:51 -0500

    EnemySprite + EnemySpriteFactory

---
+++ Sprint2/Sprites/EnemySprite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Controllers;
+
+namespace Sprint0.Sprites
+{
+    public class EnemySprite : ISprite
+    {
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        { 
+        }
+    }
+}
---
+++ Sprint2/Sprites/EnemySpriteFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint0.Sprites
+{
+    public class EnemySpriteFactory
+    {
+        private Texture2D enemySpritesheet;
+
+        private static EnemySpriteFactory instance = new EnemySpriteFactory();
+        public static EnemySpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private EnemySpriteFactory()
+        {
+
+        }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            enemySpritesheet = content.Load<Texture2D>("enemy");
+        }
+
+        public ISprite EnemySprite()
+        {
+            return new EnemySprite();
+        }
+
+        public EnemySprite CreateEnemySprite()
+        {
+            return new EnemySprite();
+        }
+
+        public EnemySprite CreateBossEnemySprite()
+        {
+            return new EnemySprite();
+        }
+    }
+}
