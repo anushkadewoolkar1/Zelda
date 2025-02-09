416beab0532441286952b191c07d9702e29d8b4f
Acapulco-1 <acapulco.1@buckeyemail.osu.edu>
2025-02-09 17:36:13 -0500

    Added fix to Attack Animations as well as a fix to Magical Shields

--- Sprint2/Sprites/LinkSprite.cs
+++ Sprint2/Sprites/LinkSprite.cs
@@ -17,39 +17,51 @@ namespace Sprint0.Sprites
         private Vector2 sourceRectangleArea;
         private int colorAdjustment;
         private bool linkDamaged;
-        private int simpleClock;
+        private int damageClock;
 
         public LinkSprite(Texture2D texture, int spriteSheetXPos, int spriteSheetYPos, int[] LinkStates)
         {
             _texture = texture;
+
+            //Sets clock to 0 when Link isn't damaged 
             linkDamaged = Convert.ToBoolean(LinkStates[3]);
             if (!linkDamaged)
             {
-                simpleClock = 0;
+                damageClock = 0;
             }
 
+            int[] sourceDimensions = AdjustAttacks(spriteSheetXPos, spriteSheetYPos, LinkStates[0], LinkStates[1]);
+
+            //Checks if Link is facing Down or Sideways and if Link has the Magical Shield
+            if (LinkStates[2] < 3 && (LinkStates[0] % 2) == 1)
+            {
+                sourceDimensions = MagicalShield(sourceDimensions, LinkStates[1], LinkStates[2]);
+            }
+            
             //Because the spritesheet is each color of link stacked on top of each other, this adds to the yposition so that the colors match 
             colorAdjustment = (LinkStates[0] / 2) * 310;
 
             switch (LinkStates[1])
             {
                 case 1:
-                    sourceRectangle = new Rectangle(16, 16, FixDirection(spriteSheetXPos, LinkStates[2]), spriteSheetYPos);
+                    sourceRectangle = new Rectangle(sourceDimensions[2], sourceDimensions[3],
+                        FixDirection(sourceDimensions[0], LinkStates[2]), sourceDimensions[1]);
                     break;
                 case 2:
-                    sourceRectangle = new Rectangle(16, 16, FixDirection(spriteSheetXPos + 17, LinkStates[2]), spriteSheetYPos + colorAdjustment);
+                    sourceRectangle = new Rectangle(sourceDimensions[2], sourceDimensions[3],
+                        FixDirection(sourceDimensions[0] + 17, LinkStates[2]), sourceDimensions[1] + colorAdjustment);
                     break;
                 case 3:
-                    sourceRectangle = new Rectangle(16, 16, FixDirection(spriteSheetXPos + 34, LinkStates[2]), spriteSheetYPos + colorAdjustment);
+                    sourceRectangle = new Rectangle(sourceDimensions[2], sourceDimensions[3],
+                        FixDirection(sourceDimensions[0] + 34, LinkStates[2]), sourceDimensions[1] + colorAdjustment);
                     break;
                 case 4:
-                    sourceRectangle = new Rectangle(16, 16, FixDirection(spriteSheetXPos + 51, LinkStates[2]), spriteSheetYPos + colorAdjustment);
+                    sourceRectangle = new Rectangle(sourceDimensions[2], sourceDimensions[3],
+                        FixDirection(sourceDimensions[0] + 51, LinkStates[2]), sourceDimensions[1] + colorAdjustment);
                     break;
                 default:
                     break;
             }
-
-            
         }
 
         public void Draw(SpriteBatch spriteBatch, Vector2 _position)
@@ -59,7 +71,7 @@ namespace Sprint0.Sprites
             {
                 spriteBatch.Draw(_texture, sourceRectangle, destinationRectangle, Color.White);
             } else {
-                switch (simpleClock % 3)
+                switch (damageClock % 3)
                 {
                     case 0:
                         spriteBatch.Draw(_texture, sourceRectangle, destinationRectangle, Color.Purple);
@@ -73,7 +85,7 @@ namespace Sprint0.Sprites
                     default:
                         break;
                 }
-                simpleClock++;
+                damageClock++;
             }
 
         }
@@ -98,5 +110,96 @@ namespace Sprint0.Sprites
             }
         }
 
+        private int[] AdjustAttacks(int xCoordinate, int yCoordinate, int frame, int facingDirection)
+        {
+            int[] sourceDimensions = {xCoordinate, yCoordinate, 16, 16};
+
+            if (yCoordinate > 46)
+            {
+                return sourceDimensions;
+            }
+
+            if (facingDirection == 0)
+            {
+                switch (frame)
+                {
+                    case 2:
+                        sourceDimensions[3] = 27;
+                        break;
+                    case 3:
+                        sourceDimensions[3] = 23;
+                        break;
+                    case 4:
+                        sourceDimensions[3] = 19;
+                        break;
+                    default:
+                        break;
+                }    
+            } else if (facingDirection == 3)
+            {
+                switch (frame)
+                {
+                    case 2:
+                        sourceDimensions[3] = 27;
+                        break;
+                    case 3:
+                        sourceDimensions[3] = 23;
+                        sourceDimensions[1] = yCoordinate - (27 - 17);
+                        break;
+                    case 4:
+                        sourceDimensions[3] = 19;
+                        sourceDimensions[1] = yCoordinate - (27 - 17);
+                        break;
+                    default:
+                        break;
+                }
+            } else
+            {
+                switch (frame)
+                {
+                    case 2:
+                        sourceDimensions[2] = 28;
+                        break;
+                    case 3:
+                        sourceDimensions[2] = 23;
+                        sourceDimensions[0] = xCoordinate + 12;
+                        break;
+                    case 4:
+                        sourceDimensions[3] = 19;
+                        sourceDimensions[0] = xCoordinate + 12 + 8;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return sourceDimensions;
+        }
+
+        private int[] MagicalShield(int[] sourceDimensions, int facingDirection, int frame)
+        {
+            switch (sourceDimensions[2])
+            {
+                case 1:
+                    sourceDimensions[0] += 288;
+                    break;
+                case 47:
+                    sourceDimensions[1] += 81;
+                    sourceDimensions[0] -= 35;
+                    break;
+                case 77:
+                    sourceDimensions[1] += 51;
+                    if (frame == 3)
+                    {
+                        sourceDimensions[0] -= 10;
+                    } else if (frame == 4)
+                    {
+                        sourceDimensions[0] -= 11;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return sourceDimensions;
+        }
     }
 }
