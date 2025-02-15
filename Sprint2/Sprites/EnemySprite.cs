480a7c52880bd698d5819890659bebeace534488
Chloe Feller <feller.75@osu.edu>
2025-02-14 22:51:19 -0500

    Adding Enemies Sprites

--- Sprint2/Sprites/EnemySprite.cs
+++ Sprint2/Sprites/EnemySprite.cs
@@ -16,14 +16,22 @@ namespace Sprint0.Sprites
         public Texture2D Texture {  get; set; }
         public int Rows { get; set; }
         public int Cols { get; set; }
+        private int StartX;
+        private int StartY;
+        private int XSize;
+        private int YSize;
         private int currentFrame;
         private int totalFrames;
 
-        public EnemySprite(Texture2D texture, int rows, int columns)
+        public EnemySprite(Texture2D texture, int rows, int columns, int startX, int startY, int xSize, int ySize)
         {
             Texture = texture;
             Rows = rows;
             Cols = columns;
+            StartX = startX;
+            StartY = startY;
+            XSize = xSize;
+            YSize = ySize;
             currentFrame = 0;
             totalFrames = Rows * Cols;
         }
@@ -43,8 +51,9 @@ namespace Sprint0.Sprites
 
         public void Draw(SpriteBatch spriteBatch, Vector2 position)
         {
-            Rectangle SourceRectangle = new Rectangle(0, 0, 16, 32);
-            Rectangle DestinationRectangle = new Rectangle((int)position.X, (int)position.Y, SourceRectangle.Width, SourceRectangle.Height);
+            int spriteSize = 32;
+            Rectangle SourceRectangle = new Rectangle(StartX, StartY, XSize, YSize);
+            Rectangle DestinationRectangle = new Rectangle((int)position.X, (int)position.Y, spriteSize, spriteSize * 2);
 
             spriteBatch.Draw(Texture, DestinationRectangle, SourceRectangle, Color.White);
             
--- Sprint2/Sprites/EnemySpriteFactory.cs
+++ Sprint2/Sprites/EnemySpriteFactory.cs
@@ -5,14 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Zelda.Enums;
 
 namespace Sprint0.Sprites
 {
     public class EnemySpriteFactory
     {
-        private Texture2D enemySpritesheet;
+        private Texture2D largeSpritesheet;
+        private Texture2D smallSpritesheet;
         private Texture2D bossSpritesheet;
         private Texture2D npcSpritesheet;
+        private int XSize;
+        private int YSize;
 
         private static EnemySpriteFactory instance = new EnemySpriteFactory();
         public static EnemySpriteFactory Instance
@@ -31,27 +35,61 @@ namespace Sprint0.Sprites
         public void LoadAllTextures(ContentManager content)
         {
             npcSpritesheet = content.Load<Texture2D>("lozNPCs");
+            largeSpritesheet = content.Load<Texture2D>("LargeEnemies");
+            smallSpritesheet = content.Load<Texture2D>("SmallEnemies");
+            bossSpritesheet = content.Load<Texture2D>("BossEnemies");
         }
 
         // change all these when the spritesheets are actually loaded in
         public EnemySprite CreateNPCSprite()
         {
-            return new EnemySprite(npcSpritesheet, 5, 1);
-        }
-        
-        public EnemySprite CreateSmallEnemySprite()
-        {
-            return new EnemySprite(npcSpritesheet, 0, 0);
+            return new EnemySprite(npcSpritesheet, 1, 1, 0, 0, 16, 32);
         }
 
-        public EnemySprite CreateLargeEnemySprite()
+        public EnemySprite CreateEnemySprite(EnemyType enemyType)
         {
-            return new EnemySprite(npcSpritesheet, 0, 0);
+            EnemySprite sprite;
+            XSize = 16;
+            YSize = 32;
+            switch(enemyType)
+            {
+                case EnemyType.OldMan:
+                    return sprite = CreateNPCSprite();
+                case EnemyType.Keese:
+                    return sprite = CreateSmallEnemySprite(enemyType, 2, 19, 0);
+                case EnemyType.Stalfos:
+                    return sprite = CreateLargeEnemySprite(enemyType, 2, 123, 18);
+                case EnemyType.Goriya:
+                    YSize = 16;
+                    return sprite = CreateLargeEnemySprite(enemyType, 6, 30, 0);
+                case EnemyType.Gel:
+                    XSize = 8;
+                    YSize = 16;
+                    return sprite = CreateSmallEnemySprite(enemyType, 2, 0, 0);
+                case EnemyType.Zol:
+                    XSize = 12;
+                    YSize = 16;
+                    return sprite = CreateLargeEnemySprite(enemyType, 2, 0, 0);
+                default:
+                    return sprite = CreateNPCSprite();
+            }
+        }
+        
+        public EnemySprite CreateSmallEnemySprite(EnemyType enemyType, int rows, int startX, int startY)
+        {
+            EnemySprite sprite;
+            return sprite = new EnemySprite(smallSpritesheet, rows, 1, startX, startY, XSize, YSize);
+        }
+
+        public EnemySprite CreateLargeEnemySprite(EnemyType enemyType, int rows, int startX, int startY)
+        {
+            EnemySprite sprite;
+            return sprite = new EnemySprite(largeSpritesheet, rows, 1, startX, startY, XSize, YSize);
         }
 
         public EnemySprite CreateBossEnemySprite()
         {
-            return new EnemySprite(npcSpritesheet, 0, 0);
+            return new EnemySprite(bossSpritesheet, 0, 0, 0, 0, XSize, YSize);
         }
     }
 }
