f17880bab7000c37e2cfc7b8613da009d5ac3561
hrblr <aidanroley@icloud.com>
2025-02-28 19:36:29 -0500

    block changes

--- Sprint2/GameObjects/InvisibleBlock.cs
+++ Sprint2/GameObjects/InvisibleBlock.cs
@@ -19,7 +19,12 @@ public class InvisibleBlock : Block
             float scaleFactor = 0.3f;
 
             //Use the transparent block texture 
-            spriteBatch.Draw(textures[0], pixelPosition, null, Color.White, 0f, Vector2.Zero, scaleFactor, SpriteEffects.None, 0f);
+            //spriteBatch.Draw(textures[0], pixelPosition, null, Color.White, 0f, Vector2.Zero, scaleFactor, SpriteEffects.None, 0f);
+            Texture2D solidColor = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
+            solidColor.SetData(new[] { Color.White }); // Make it a solid white
+
+            // Draw it at the correct position, scaling it
+            spriteBatch.Draw(solidColor, pixelPosition, null, Color.Red, 0f, Vector2.Zero, scaleFactor, SpriteEffects.None, 0f);
         }
     }
 }
