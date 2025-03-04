440f57f8eb90b99129861022ecf08ff0f85d4571
Chloe Feller <feller.75@osu.edu>
2025-03-04 11:49:46 -0500

    Link teleports 2 blocks away from enemy based on collision side

--- Sprint2/Collision/LinkEnemyCollisionHandler.cs
+++ Sprint2/Collision/LinkEnemyCollisionHandler.cs
@@ -20,21 +20,27 @@ namespace Sprint0.Collision
             // link takes damage, enemies will immediately walk the opposite direction (doesn't matter whether they continue to walk in that direction or not tho)
 
             // start the invulnerability, change later to TakeDamage() method like in another collision case if that's added
-            link.StartInvulnerability();
+            // push link back 3 blocks
+            link.ChangeState(new LinkDamagedState(link, link.currentDirection, link.currentState));
+            link.currentState.Enter();
 
-            switch(side)
+            switch (side)
             {
                 case CollisionSide.Left:
                     enemy.ChangeDirection(Direction.Right);
+                    link.Position = new Microsoft.Xna.Framework.Vector2(link.Position.X - 38, link.Position.Y);
                     break;
                 case CollisionSide.Right:
                     enemy.ChangeDirection(Direction.Left);
+                    link.Position = new Microsoft.Xna.Framework.Vector2(link.Position.X + 38, link.Position.Y);
                     break;
                 case CollisionSide.Top:
                     enemy.ChangeDirection(Direction.Down);
+                    link.Position = new Microsoft.Xna.Framework.Vector2(link.Position.X, link.Position.Y - 38);
                     break;
                 case CollisionSide.Bottom:
                     enemy.ChangeDirection(Direction.Up);
+                    link.Position = new Microsoft.Xna.Framework.Vector2(link.Position.X, link.Position.Y + 38);
                     break;
             }
         }
