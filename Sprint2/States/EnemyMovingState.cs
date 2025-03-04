4b7a56df6b30ba372168c5d1ed6d3f45ac0f45e3
Chloe Feller <feller.75@osu.edu>
2025-03-04 00:04:08 -0500

    Refactoring some of enemy's movement code to better suit with collisions

--- Sprint2/States/EnemyMovingState.cs
+++ Sprint2/States/EnemyMovingState.cs
@@ -46,6 +46,8 @@ namespace Sprint0.States
 
             enemy.Speed = 100f;
 
+            int random = RandomNumberGenerator.GetInt32(-1, 2);
+
             switch (enemy.enemyType)
             {
                 case EnemyType.OldMan:
@@ -53,87 +55,37 @@ namespace Sprint0.States
                     enemy.Speed = 0;
                     break;
                 case EnemyType.Keese:
-                    if (timer >= 0 && timer < 25)
+                    if (enemy.Direction == Direction.Up || enemy.Direction == Direction.Down)
                     {
-                        move = new Vector2(1, RandomNumberGenerator.GetInt32(-1, 2));
-                    }
-                    else if (timer >= 25 && timer < 50)
+                        move = MoveDirection(enemy.Direction);
+                        move.X = random;
+                    } else
                     {
-                        move = new Vector2(1, RandomNumberGenerator.GetInt32(-1, 2));
+                        move = MoveDirection(enemy.Direction);
+                        move.Y = random;
                     }
-                    else if (timer >= 50 && timer < 75)
-                    {
-                        move = new Vector2(-1, RandomNumberGenerator.GetInt32(-1, 2));
-                    }
-                    else if (timer >= 75 && timer < 100)
-                    {
-                        move = new Vector2(-1, RandomNumberGenerator.GetInt32(-1, 2));
-                    }
+
                     break;
                 case EnemyType.Stalfos:
-                    if (timer >= 0 && timer < 25)
+                    if(timer == 0 || timer == 50)
                     {
-                        move = MoveDirection(Direction.Right);
-                    } else if (timer >= 25 && timer < 50)
-                    {
-                        move = MoveDirection(Direction.Up);
-                    } else if (timer >= 50 && timer < 75)
-                    {
-                        move = MoveDirection(Direction.Left);
-                    } else if (timer >= 75 && timer < 100)
-                    {
-                        move = MoveDirection(Direction.Down);
-                    } 
+                        enemy.ChangeDirection((Direction)random);
+                    }
+
+                    move = MoveDirection(enemy.Direction);
                     break;
                 case EnemyType.Goriya:
                     // refactor this later, this is pretty hard to look at tbh
                     Boolean moving = true;
-                    if (timer == 0)
+                    if(timer == 0 || timer == 25 || timer == 50)
                     {
-                        int random = RandomNumberGenerator.GetInt32(-1, 2);
-                        if (random == -1)
-                        {
-                            enemy.ChangeDirection(Direction.Up);
-                        }
-                        else if (random == 1)
-                        {
-                            enemy.ChangeDirection(Direction.Down);
-                        }
-                    }
-                    else if (timer == 25)
-                    {
-                        int random = RandomNumberGenerator.GetInt32(-1, 2);
-                        if (random == -1)
-                        {
-                            enemy.ChangeDirection(Direction.Right);
-                        }
-                        else if (random == 1)
-                        {
-                            enemy.ChangeDirection(Direction.Left);
-                        }
-                    }
-                    else if (timer == 50)
-                    {
-                        int random = RandomNumberGenerator.GetInt32(-1, 2);
-                        if (random == -1)
-                        {
-                            enemy.ChangeDirection(Direction.Up);
-                        }
-                        else if (random == 1)
-                        {
-                            enemy.ChangeDirection(Direction.Down);
-                        }
-
-                    }
-                    else if (timer == 75)
+                        enemy.ChangeDirection((Direction)random);
+                    } else if (timer == 75)
                     {
                         enemy.SpawnProjectile();
-                    }
-                    else if (timer > 75 && timer < 100)
-                    {
                         moving = false;
-
                     }
+
                     if (moving)
                     {
                         move = MoveDirection(enemy.Direction);
@@ -147,12 +99,9 @@ namespace Sprint0.States
                     if ((timer >= 0 && timer < 25) || (timer >= 50 && timer < 75))
                     {
                         move = new Vector2(0, 0);
-                    } else if (timer >= 25 && timer < 50)
-                    {
-                        move = new Vector2(RandomNumberGenerator.GetInt32(-1, 2), 0);
                     } else
                     {
-                        move = new Vector2(0, RandomNumberGenerator.GetInt32(-1, 2));
+                        move = MoveDirection(enemy.Direction);
                     }
                     break;
                 case EnemyType.Zol:
@@ -160,13 +109,9 @@ namespace Sprint0.States
                     {
                         move = new Vector2(0, 0);
                     }
-                    else if (timer >= 25 && timer < 50)
-                    {
-                        move = new Vector2(RandomNumberGenerator.GetInt32(0, 2), 0);
-                    }
                     else
                     {
-                        move = new Vector2(0, RandomNumberGenerator.GetInt32(0, 2));
+                        move = MoveDirection(enemy.Direction);
                     }
                     break;
                 case EnemyType.Trap:
@@ -268,7 +213,7 @@ namespace Sprint0.States
             enemy.Move(move, gameTime);
         }
 
-        private Vector2 MoveDirection(Direction dir)
+        public Vector2 MoveDirection(Direction dir)
         {
             switch(dir)
             {
