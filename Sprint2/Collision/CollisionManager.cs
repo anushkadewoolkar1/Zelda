749738e4eff95e8c19a5d00b72ef609764ab825f
Acapulco-1 <acapulco.1@buckeyemail.osu.edu>
2025-03-05 14:15:47 -0500

    Delete Projectile Collision Handler

--- Sprint2/Collision/CollisionManager.cs
+++ Sprint2/Collision/CollisionManager.cs
@@ -138,7 +138,6 @@ public class CollisionManager
         } else if (objA is ProjectileSprite)
         {
             return;
-            //DOESN'T WORK = collisionHandler = new ProjectileCollisionHandler();
         }
 
 
