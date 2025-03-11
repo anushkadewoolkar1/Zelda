8fcee027726be4b3b9d504184ba15544b3abf6c3
PaulPaciorek <paciorek.15@buckeyemail.osu.edu>
2025-03-11 17:39:54 -0400

    GamePadController completed implementation

--- Sprint2/Controllers/GamePadController.cs
+++ Sprint2/Controllers/GamePadController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -14,62 +15,166 @@ namespace Sprint0.Controllers
     {
         private readonly Dictionary<GamePadButtonEnums, ICommand> buttonCommandMap;
         private readonly Dictionary<GamePadJoystickEnums, ICommand> joystickCommandMap;
-        private readonly Dictionary<GamePadTriggerEnums, ICommand> triggerCommandMap;
         private readonly Dictionary<GamePadDPadEnums, ICommand> dPadCommandMap;
         private GamePadButtonEnums lastButton;
         private GamePadJoystickEnums lastJoystick;
-        private GamePadTriggerEnums lastTrigger;
         private GamePadDPadEnums lastDPad;
 
-        // Parameters of constructor are four dictionaries, one for buttons, joysticks, triggers, and DPad respectively:
-        public GamePadController(Dictionary<GamePadButtonEnums, ICommand> buttonMap, Dictionary<GamePadJoystickEnums, ICommand> joystickMap, Dictionary<GamePadTriggerEnums, ICommand> triggerMap, Dictionary<GamePadDPadEnums, ICommand> dPadMap)
+        //Used as diagonal value of joystick:
+        private const float HALF_SQRT_TWO = (float).7071;
+
+        // Parameters of constructor are three dictionaries, one for buttons, joysticks, and DPad respectively:
+        public GamePadController(Dictionary<GamePadButtonEnums, ICommand> buttonMap, Dictionary<GamePadJoystickEnums, ICommand> joystickMap, Dictionary<GamePadDPadEnums, ICommand> dPadMap)
         {
             buttonCommandMap = buttonMap;
             joystickCommandMap = joystickMap;
-            triggerCommandMap = triggerMap;
             dPadCommandMap = dPadMap;
+
             // Assign default values for lastInputs upon construction:
             lastButton = GamePadButtonEnums.None;
             lastJoystick = GamePadJoystickEnums.None;
-            lastTrigger = GamePadTriggerEnums.None;
             lastDPad = GamePadDPadEnums.None;
         }
 
         public override void Update()
         {
-            KeyboardState state = Keyboard.GetState();
+            bool anyButtonPressed = false, anyJoystickMoved = false, anyDPadPressed = false;
+            GamePadState state = GamePad.GetState(0);
 
-            Keys[] pressedKeys = state.GetPressedKeys();
-
-            foreach (Keys key in pressedKeys)
+            // Using if statements as I am unable to iterate through the GamePadState structs, we can talk later about a potential alternative:
+            if (state.Buttons.A.Equals(ButtonState.Pressed))
             {
-                // if key in map execute
-                if (_keyCommandMap.ContainsKey(key))
+                if (lastButton != GamePadButtonEnums.DownBtn)
                 {
-                    if (key != lastInput)
-                    {
-                        _keyCommandMap[key].Execute();
-                    }
-                    // Update lastInput if input is valid command: (PP)
-                    lastInput = key;
+                    buttonCommandMap[GamePadButtonEnums.DownBtn].Execute();
+                    lastButton = GamePadButtonEnums.DownBtn;
                 }
+                anyButtonPressed = true;
+            }
+            if (state.Buttons.B.Equals(ButtonState.Pressed))
+            {
+               if (lastButton != GamePadButtonEnums.RightBtn)
+               {
+                    buttonCommandMap[GamePadButtonEnums.RightBtn].Execute();
+                    lastButton = GamePadButtonEnums.RightBtn;
+                }
+                anyButtonPressed = true;
             }
 
-            if (pressedKeys.Length > 0)
+
+            /*
+             * Uncomment once pause menu and options menu are implemented:
+             * 
+            if (state.Buttons.Start.Equals(ButtonState.Pressed))
             {
-                lastKeyIdle = false;
+               if (lastButton != GamePadButtonEnums.Start)
+               {
+                    buttonCommandMap[GamePadButtonEnums.Start].Execute();
+                    lastButton = GamePadButtonEnums.Start;
+                }
+                anyButtonPressed = true;
+            }
+            if (state.Buttons.Select.Equals(ButtonState.Pressed))
+            {
+               if (lastButton != GamePadButtonEnums.Select)
+               {
+                    buttonCommandMap[GamePadButtonEnums.Select].Execute();
+                    lastButton = GamePadButtonEnums.Select;
+                }
+                anyButtonPressed = true;
+            }
+            */
+
+            // If no buttons are pressed, nothing should happen:
+            if (!anyButtonPressed)
+            {
+                lastButton = GamePadButtonEnums.None;
+            }
+
+            // Declare variables to make remaining code more simple to read:
+            float joystickX = state.ThumbSticks.Left.X, joystickY = state.ThumbSticks.Left.Y;
+
+            // If |X or Y position| of joystick is greater than sqrt(2)/2, then it must be biased in that direction:
+            if (joystickX < (-1 * HALF_SQRT_TWO))
+            {
+                if (lastJoystick != GamePadJoystickEnums.LeftLJS)
+                {
+                    joystickCommandMap[GamePadJoystickEnums.LeftLJS].Execute();
+                    lastJoystick = GamePadJoystickEnums.LeftLJS;
+                }
+                anyJoystickMoved = true;
+            } else if (joystickX >= HALF_SQRT_TWO)
+            {
+                if (lastJoystick != GamePadJoystickEnums.RightLJS)
+                {
+                    joystickCommandMap[GamePadJoystickEnums.RightLJS].Execute();
+                    lastJoystick = GamePadJoystickEnums.RightLJS;
+                }
+                anyJoystickMoved = true;
+            } else if (joystickY < (-1 * HALF_SQRT_TWO))
+            {
+                if (lastJoystick != GamePadJoystickEnums.DownLJS)
+                {
+                    joystickCommandMap[GamePadJoystickEnums.DownLJS].Execute();
+                    lastJoystick = GamePadJoystickEnums.DownLJS;
+                }
+                anyJoystickMoved = true;
+            } else if (joystickX >= HALF_SQRT_TWO)
+            {
+                if (lastJoystick != GamePadJoystickEnums.UpLJS)
+                {
+                    joystickCommandMap[GamePadJoystickEnums.UpLJS].Execute();
+                    lastJoystick = GamePadJoystickEnums.UpLJS;
+                }
+                anyJoystickMoved = true;
+            } else
+            {
+                lastJoystick = GamePadJoystickEnums.None;
             }
 
 
-            // If no input is given, reset lastInput to Keys.None (default state), and return Link to default idle state: (PP)
-            if (pressedKeys.Length == 0)
+            // Update DPad State:
+            if (state.DPad.Down.Equals(ButtonState.Pressed))
             {
-                lastInput = Keys.None;
-                if (lastKeyIdle == false)
+                if (lastDPad != GamePadDPadEnums.Down)
                 {
-                    _keyCommandMap[Keys.None].Execute();
-                    lastKeyIdle = true;
+                    dPadCommandMap[GamePadDPadEnums.Down].Execute();
+                    lastDPad = GamePadDPadEnums.Down;
                 }
+                anyDPadPressed = true;
+            }
+            if (state.DPad.Up.Equals(ButtonState.Pressed))
+            {
+                if (lastDPad != GamePadDPadEnums.Up)
+                {
+                    dPadCommandMap[GamePadDPadEnums.Up].Execute();
+                    lastDPad = GamePadDPadEnums.Up;
+                }
+                anyDPadPressed = true;
+            }
+            if (state.DPad.Left.Equals(ButtonState.Pressed))
+            {
+                if (lastDPad != GamePadDPadEnums.Left)
+                {
+                    dPadCommandMap[GamePadDPadEnums.Left].Execute();
+                    lastDPad = GamePadDPadEnums.Left;
+                }
+                anyDPadPressed = true;
+            }
+            if (state.DPad.Right.Equals(ButtonState.Pressed))
+            {
+                if (lastDPad != GamePadDPadEnums.Right)
+                {
+                    dPadCommandMap[GamePadDPadEnums.Right].Execute();
+                    lastDPad = GamePadDPadEnums.Right;
+                }
+                anyDPadPressed = true;
+            }
+
+            // If neither joystick or DPad is pressed, reset Link to idle state:
+            if (!anyJoystickMoved && !anyDPadPressed)
+            {
+                dPadCommandMap[GamePadDPadEnums.None].Execute();
             }
         }
     }
