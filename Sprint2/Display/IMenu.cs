ac68da643b05f9ef5768f7ea490390ff3bc59412
PaulPaciorek <paciorek.15@buckeyemail.osu.edu>
2025-03-20 15:20:11 -0400

    Small update to IMenu, added methods and comments

--- Sprint2/Display/IMenu.cs
+++ Sprint2/Display/IMenu.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Sprint0.Display;
 
+
 namespace Sprint0.Display
 {
-
+    // Interface for menus, extending the behavior of a display (PP):
     public abstract class IMenu : IDisplay
     {
         // Called once per frame to update sprites
         public abstract void Update(GameTime gameTime);
 
-        // Draws sprite on the screen
+        // Draws sprites on the screen
         public abstract void Draw(SpriteBatch spriteBatch);
 
+        // Executes a state modification based on command definition:
+        public abstract void LoadCommand(ICommand command);
+
+        // Holds the current state of level to be brought back later:
+        public abstract void PauseLevel(Level level);
+
+        // Maybe we need this? idk yet: 
+        // public abstract void UnpauseLevel(Level level);
     }
 }
