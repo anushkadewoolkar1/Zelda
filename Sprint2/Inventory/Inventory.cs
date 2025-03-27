2a691167f75dfefca0f67f174b34531ac276fa86
Aidan Roley <aidanroley@icloud.com>
2025-03-27 13:33:44 -0400

    inventory content

---
+++ Sprint2/Inventory/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprint0.CollisionHandling;
+
+namespace ZeldaGame.Zelda.Sprint2.Inventory
+{
+    public class Inventory
+    {
+        private Texture2D _backgroundTexture;
+
+        public Inventory(ContentManager content, List<IGameObject> gameObjects)
+        {
+            _backgroundTexture = content.Load<Texture2D>("PauseScreen");
+        }
+    }
+}
