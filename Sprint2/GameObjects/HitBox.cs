b558a3a3e120402de2c1985d230431ea876038d5
Acapulco-1 <acapulco.1@buckeyemail.osu.edu>
2025-04-04 01:36:56 -0400

    Added better hit detection for Link's attack and adjusted Link properly when he transitions into another room.

--- Sprint2/GameObjects/HitBox.cs
+++ Sprint2/GameObjects/HitBox.cs
@@ -11,7 +11,7 @@ public class HitBox : IGameObject
 {
     private Rectangle hitRectangle;
     private Vector2 _direction;
-    private int SCALE = 8;
+    private int SCALE = 32;
     public int scalePropertyHit
     {
         get
@@ -25,19 +25,19 @@ public class HitBox : IGameObject
         switch (direction)
         {
             case Direction.Left:
-                hitRectangle = new Rectangle((int)startPosition.X - (SCALE), (int)startPosition.Y, SCALE, SCALE * 2);
+                hitRectangle = new Rectangle((int)startPosition.X - (SCALE), (int)startPosition.Y, SCALE, SCALE);
                 _direction = new Vector2(-1, 0);
                 break;
             case Direction.Right:
-                hitRectangle = new Rectangle((int)startPosition.X + (SCALE * 2), (int)startPosition.Y, SCALE, SCALE * 2);
+                hitRectangle = new Rectangle((int)startPosition.X + (SCALE / 2), (int)startPosition.Y, SCALE, SCALE);
                 _direction = new Vector2(1, 0);
                 break;
             case Direction.Up:
-                hitRectangle = new Rectangle((int)startPosition.X, (int)startPosition.Y - SCALE, SCALE * 2, SCALE);
+                hitRectangle = new Rectangle((int)startPosition.X, (int)startPosition.Y - (SCALE), SCALE, SCALE);
                 _direction = new Vector2(0, -1);
                 break;
             default:
-                hitRectangle = new Rectangle((int)startPosition.X + (SCALE * 2), (int)startPosition.Y, SCALE * 2, SCALE);
+                hitRectangle = new Rectangle((int)startPosition.X, (int)startPosition.Y + (SCALE / 2), SCALE, SCALE);
                 _direction = new Vector2(0, 1);
                 break;
         }
